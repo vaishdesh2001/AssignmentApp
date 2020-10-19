212c79e10f1bf576555a2212bbd54df6c6f9f6f4
mishra37 <69707888+mishra37@users.noreply.github.com>
2020-10-19 12:57:37 -0500

    Update AssignmentScheduler.java

--- AssignmentApp/AssignmentScheduler.java
+++ AssignmentApp/AssignmentScheduler.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 // Name: Ayushi Mishra
 // Email: mishra37@wisc.edu
 // Team: LB
+// Role: Backend Developer
 // TA: Divyanshu Saxena
 // Lecturer: Florian
 // Notes to Grader: <optional extra notes>
@@ -11,9 +12,9 @@ import java.util.NoSuchElementException;
  * 
  * @author ayushi
  *
- *         This class implements Red Black Tree data structure, which stores objects of Assignment
- *         class as nodes
- * @param <T>
+ * This class implements Red Black Tree data structure, which stores objects of Assignment
+ * class as nodes
+ * @param T - Assignment
  */
 public class AssignmentScheduler extends RedBlackTree<Assignment> {
 
