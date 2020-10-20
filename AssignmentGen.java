e984d5deed6dd7d507a19fb810c834403659e1a7
shrey20 <53436076+shrey20@users.noreply.github.com>
2020-10-20 15:35:25 +0530

    Update AssignmentGen.java

--- AssignmentGen.java
+++ AssignmentGen.java
@@ -9,19 +9,23 @@
 import java.util.Random;
 import java.io.FileWriter;
 public class AssignmentGen {
- Assignment[] assign = new Assignment[120]; 
+ Assignment[] assign = new Assignment[500]; //creates a new array of type assignment
    String[] types = {"Monthly Quiz", "Weekly Project", "Group Project", "Homework", "Midterm", "Final",
       "Discussion", "Participation", "Research Paper", "Presentation"};
  String[] courseNames = {"ECOL 110", "FINANCE 300", "CS 400", "MATH 431", "ACC 300", "CS 350",
       "ESL 118", "CS 320", "HIST 110", "CS 240", "CS 577"};
 
-
+/**
+ * Method to generate and load data for the assignment scheduler project.
+ * Takes the array of courseNames and types and generates random points and date 
+ * to populate the assign array of assignment type.
+ */
 public void Genfile(){
 Random points = new Random();
 Random days = new Random();
 int i =0;
 for (int x = 0; x < courseNames.length; x++){
-    for (int y =0; y < types.length; y++){
+    for (int y =0; y < types.length; y++){ //loop for iterating through the course names and types.
         int day = days.nextInt(31)+1;
         Date dueDate = new Date(day, 10, 2020);
         int point = (points.nextInt(20)+1)*5;
@@ -29,13 +33,13 @@ for (int x = 0; x < courseNames.length; x++){
         i ++;
     }
 }
-System.out.println("Loading assignments: " + i);
+System.out.println("Loading assignments: " + i); // a print statement to ensure the data is loaded
 try {
       FileWriter fw = new FileWriter("src/AssignementScheduler.txt");
       fw.write("Due date, Assignment type, Course, Points worth\n");
       for (int a = 0; a < i; a++) {
         fw.write(assign[a].getDate() + "," + assign[a].getAssignName() + ","
-            + assign[a].getCourseName() + "," + assign[a].getPoints() + "\n");
+            + assign[a].getCourseName() + "," + assign[a].getPoints() + "\n"); //comma seperated file.
       }
       fw.close();
     } catch (Exception e) {
@@ -44,7 +48,6 @@ try {
   }
 
 }
-
 
 
 
