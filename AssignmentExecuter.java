bdc581426e1f2f53204f0b1bdaa4ceee7548529e
shrey20 <53436076+shrey20@users.noreply.github.com>
2020-10-20 15:12:02 +0530

    Update AssignmentExecuter.java

--- AssignmentExecuter.java
+++ AssignmentExecuter.java
@@ -1,8 +1,8 @@
 // --== CS400 File Header Information ==--
-// Name: Vaishnavi Deshpande
-// Email: vvdeshpande@wisc.edu
+// Name: Shreyans Sakhlecha
+// Email: sakhlecha@wisc.edu
 // Team: LB
-// Role: Data Wrangler 2
+// Role: Data Wrangler 1
 // TA: Divyanshu Saxena
 // Lecturer: Gary Dahl
 // Notes to Grader: <optional extra notes>
@@ -10,9 +10,10 @@
 
 public class AssignmentExecuter {
   public static void main(String args[]) {
-    System.out.println("now generating file");
-    AssignmentGen fileGen = new AssignmentGen();
-    fileGen.Genfile();
-    
-  }
+      System.out.println("now generating file");
+      AssignmentGen fileGen = new AssignmentGen();
+      fileGen.Genfile();  
+      AssignmentScheduler runner = new AssignmentScheduler();
+      FrontEndInterface.beginPrompt(runner); 
+    }
 }
